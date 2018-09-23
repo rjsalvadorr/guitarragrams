2db9899ef636aac8efb35507733a7cede80eadbe
RJ Salvador <randolph.salvador@gmail.com>
2018-09-23 14:23:25 -0400

    Linked data with vue components

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
   <div id="app">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr>
+    <h1>Debug info</h1>
+    <pre>
+      <code>{{ `diagrams = ${JSON.stringify(diagrams, null, 2)}` }}</code>
+    </pre>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
+import chordDiagramsOpen from "./data/chord-diagrams-open.json";
+import chordDiagrams3string from "./data/chord-diagrams-3string.json";
 
 export default {
   name: "app",
   components: {
     HelloWorld
+  },
+  data: function() {
+    return {
+      diagrams: {
+        open: chordDiagramsOpen,
+        '3string': chordDiagrams3string
+      }
+    }
   }
 };
+
 </script>
 
 <style lang="scss">
@@ -23,5 +39,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+pre,
+code {
+  text-align: left;
 }
 </style>
